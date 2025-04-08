efcd65fd635089f56415061c798c9cf3a81d6d95
Loong <1031313955@qq.com>
2025-04-08 20:06:45 +0800

    splitbutton

--- imguiex_split_layout.h
+++ imguiex_split_layout.h
@@ -19,29 +19,27 @@ class SplitLayout : public Layout {
         float splitPos = (orientation_ == Horizontal) ? size.x * split_ration_ : size.y * split_ration_;
 
         if (orientation_ == Horizontal) {
-            ImGui::BeginChild(left_or_top_->Name().c_str(), ImVec2(splitPos - 4, 0), true);
+            ImGui::BeginChild(left_or_top_->Name().c_str(), ImVec2(splitPos, 0), true);
             left_or_top_->Render();
             ImGui::EndChild();
 
+            // ImGui::SameLine();
             ImGui::SameLine();
-
-            // // === 拖拉条 ===
-            // ImGui::PushStyleColor(ImGuiCol_Button, IM_COL32(128, 128, 128, 255));
-            // ImGui::PushStyleColor(ImGuiCol_ButtonHovered, IM_COL32(160, 160, 160, 255));
-            // ImGui::PushStyleColor(ImGuiCol_ButtonActive, IM_COL32(192, 192, 192, 255));
             ImVec2 old = ImGui::GetCursorScreenPos();
-            ImGui::SetCursorScreenPos(ImVec2(splitPos - 4, 0));
-            ImGui::Button("##splitter", ImVec2(6.0f, -1.0f));
+            ImVec2 padding = ImGui::GetStyle().ItemSpacing;
 
-            ImGui::SetCursorScreenPos(old);
+            ImGui::SetCursorScreenPos(ImVec2(old.x - padding.x, old.y));
+            ImGui::Button("##splitter", ImVec2(padding.x, -1.0f));
+
             if (ImGui::IsItemActive() && ImGui::IsMouseDragging(0)) {
                 float delta = ImGui::GetIO().MouseDelta.x / size.x;
                 split_ration_ += delta;
                 split_ration_ = std::clamp(split_ration_, 0.1f, 0.9f);
             }
-            // ImGui::PopStyleColor(3);
 
             ImGui::SameLine();
+
+            ImGui::SetCursorScreenPos(old);
             ImGui::BeginChild(right_or_bottom_->Name().c_str(), ImVec2(0, 0), true);
             right_or_bottom_->Render();
             ImGui::EndChild();
@@ -51,17 +49,19 @@ class SplitLayout : public Layout {
             left_or_top_->Render();
             ImGui::EndChild();
 
-            // // === 拖拉条 ===
-            // ImGui::PushStyleColor(ImGuiCol_Button, IM_COL32(128, 128, 128, 255));
-            // ImGui::PushStyleColor(ImGuiCol_ButtonHovered, IM_COL32(160, 160, 160, 255));
-            // ImGui::PushStyleColor(ImGuiCol_ButtonActive, IM_COL32(192, 192, 192, 255));
-            ImGui::InvisibleButton("##splitter", ImVec2(-1.0f, 6.0f));
+            ImVec2 old = ImGui::GetCursorScreenPos();
+            ImVec2 padding = ImGui::GetStyle().ItemSpacing;
+
+            ImGui::SetCursorScreenPos(ImVec2(old.x, old.y - padding.y));
+           
+            ImGui::InvisibleButton("##splitter", ImVec2(-1.0f, padding.y));
             if (ImGui::IsItemActive() && ImGui::IsMouseDragging(0)) {
                 float delta = ImGui::GetIO().MouseDelta.y / size.y;
                 split_ration_ += delta;
                 split_ration_ = std::clamp(split_ration_, 0.1f, 0.9f);
             }
-            // ImGui::PopStyleColor(3);
+
+            ImGui::SetCursorScreenPos(old);
 
             ImGui::BeginChild(right_or_bottom_->Name().c_str(), ImVec2(0, 0), true);
             right_or_bottom_->Render();
